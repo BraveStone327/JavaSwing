7d3ee5b343ef10f5dc2b524cac3142a0bd74129f
BraveStone327 <whoamiagent007@gmail.com>
2019-03-19 01:16:49 +0600

    Java Swing all excercise

---
+++ src/excercise/example.java
@@ -0,0 +1,19 @@
+
+package excercise;
+import java.util.Scanner;
+
+public class example {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        float d = 2.2f;
+        System.out.println("float = "+d);
+        for(int i=1;i<3;i++){
+            for(int j=0;j<2;j++){
+                System.out.print("J");
+            }
+            System.out.println("V");
+        }
+    }
+    
+}
